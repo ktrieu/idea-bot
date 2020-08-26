7c88013edbdd1610c6feff6945499c18c5ec9038
Kevin Trieu <kevin.trieu5813@gmail.com>
2020-08-26 13:40:23 -0400

    Add channel restriction and command detection

--- bot.py
+++ bot.py
@@ -6,13 +6,22 @@ load_dotenv()
 
 client = discord.Client()
 
+ALLOWED_CHANNELS = {"random"}
+
+COMMAND = "!idea"
+
+
+def should_respond(message):
+    return message.channel.name in ALLOWED_CHANNELS and message.content == COMMAND
+
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    print(message.content)
+    if should_respond(message):
+        print(message)
 
 
 client.run(os.environ.get("DISCORD_TOKEN"))

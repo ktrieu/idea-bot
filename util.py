2ad84cfc396077efd95938c1cc8173e04867ab4d
Kevin Trieu <kevin.trieu5813@gmail.com>
2021-01-01 18:02:56 -0500

    Make logging format a bit nicer

--- util.py
+++ util.py
@@ -5,6 +5,14 @@ import sys
 def create_logger(name):
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
-    logger.addHandler(logging.StreamHandler(sys.stdout))
-    logger.addHandler(logging.FileHandler(f"{name}.log"))
+    sysout_handler = logging.StreamHandler(sys.stdout)
+    sysout_handler.setFormatter(
+        logging.Formatter(r"%(name)-12s: %(levelname)-8s %(message)s")
+    )
+    file_handler = logging.FileHandler(f"{name}.log")
+    file_handler.setFormatter(
+        logging.Formatter(r"%(name)-12s: %(levelname)-8s %(message)s")
+    )
+    logger.addHandler(sysout_handler)
+    logger.addHandler(file_handler)
     return logger

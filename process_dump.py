e02770ec4189a183fbc280ed850450ff78617508
Kevin Trieu <kevin.trieu5813@gmail.com>
2021-08-26 21:25:10 -0400

    Make the titles dump a little bit nicer

--- process_dump.py
+++ process_dump.py
@@ -8,9 +8,8 @@ if __name__ == "__main__":
         with open("wp_dump.xml", "r", encoding="utf-8") as wp_dump_file:
             dump_file_text = wp_dump_file.read()
             # XML parsing is for communists
-            titles_file.writelines(
-                [
-                    html.unescape(match).rstrip("\n") + "\n"
-                    for match in re.findall(TITLE_REGEX, dump_file_text)
-                ]
-            )
+            for match in re.findall(TITLE_REGEX, dump_file_text):
+                if match == "":
+                    continue
+                cleaned_match = html.unescape(match.rstrip("\n"))
+                titles_file.write(cleaned_match + "\n")

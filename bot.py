1fb64a635c25a6a6b7afea52f3869ad5934f1c90
Kevin Trieu <kevin.trieu5813@gmail.com>
2023-07-11 21:19:31 -0400

    Add intents and fix event loop to deal with breaking Discord.py changes

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ load_dotenv()
 ALLOWED_CHANNELS = {"secret-channel-name", "beyond-ideas"}
 TEST_SERVER_ID = 748228407472423015
 MATHNEWS_SERVER_ID = 739575273443426305
-ALLOWED_SERVER_IDS = {TEST_SERVER_ID, MATHNEWS_SERVER_ID}
+ALLOWED_SERVER_IDS = {TEST_SERVER_ID}
 
 COMMAND = "!idea"
 
@@ -25,13 +25,16 @@ RESP_CHECK_INTERVAL_S = 1
 
 class IdeaBotClient(discord.Client):
     def __init__(self):
-        super().__init__()
+        intents = discord.Intents.default()
+        intents.messages = True
+        intents.message_content = True
+
+        super().__init__(intents=intents)
         self.logger = util.create_logger("idea-bot")
         parent_conn, child_conn = Pipe()
         self.conn = parent_conn
         self.generator_process = GeneratorProcess(conn=child_conn)
         self.generator_process.start()
-        self.loop.create_task(self.check_responses())
 
     def should_respond(self, message):
         return (
@@ -88,10 +91,16 @@ class IdeaBotClient(discord.Client):
             await asyncio.sleep(RESP_CHECK_INTERVAL_S)
 
 
-if __name__ == "__main__":
+async def main():
     print("Creating client...")
     client = IdeaBotClient()
-    print("Starting bot...")
-    client.run(os.environ.get("DISCORD_TOKEN"))
-    print("Terminating worker process...")
-    client.terminate_worker_process()
+    async with client:
+        print("Starting bot...")
+        client.loop.create_task(client.check_responses())
+        await client.start(os.environ.get("DISCORD_TOKEN"))
+        print("Terminating worker process...")
+        client.terminate_worker_process()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
